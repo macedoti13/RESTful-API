6c64e6dd973ea7493aa017772c6b7451894f640c
Thiago Macedo <thialmacedo@gmail.com>
2022-04-13 19:40:14 -0300

    req parse arguments

--- web_application.py
+++ web_application.py
@@ -19,4 +19,20 @@ def abort_if_company_doesnt_exist(company_id):
     if company_id not in df.index:
         abort(404, message=f'Company {company_id} doesn\'t exist')
 
+parser = reqparse.RequestParser()
+parser.add_argument('Symbol')
+parser.add_argument('Name')
+parser.add_argument('Sector')
+parser.add_argument('Price')
+parser.add_argument('Price/Earnings')
+parser.add_argument('Dividend Yield')
+parser.add_argument('Earnings/Share') 
+parser.add_argument('52 Week Low') 
+parser.add_argument('52 Week High') 
+parser.add_argument('Market Cap') 
+parser.add_argument('EBITDA')
+parser.add_argument('Price/Sales') 
+parser.add_argument('Price/Book') 
+parser.add_argument('SEC Filings')
+
 app.run(debug=True)
